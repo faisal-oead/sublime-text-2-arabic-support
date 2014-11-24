6e2344abd8f48924c28e53ff527de023aa51534b
faisal-oead <fafagold@gmail.com>
2014-11-24 22:59:30 +0300

    new file:   Context.sublime-menu 	new file:   Default (Linux).sublime-keymap 	new file:   Default (OSX).sublime-keymap 	new file:   Default (Windows).sublime-keymap 	new file:   Default.sublime-commands 	new file:   Main.sublime-menu 	new file:   arabic_reshaper.py 	new file:   arabic_reshaper.pyc 	new file:   rtl.py 	new file:   rtl.pyc

---
+++ rtl.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# This work is licensed under the GNU Public License (GPL).
+# To view a copy of this license, visit http://www.gnu.org/copyleft/gpl.html
+# Written by praveen vijayan (praveenvijayan)
+# tweaked by faisal oead
+
+import sublime, sublime_plugin, sys
+import arabic_reshaper
+
+class bidiCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		region = sublime.Region(0, self.view.size())
+		bidiRegion(region, self.view, edit)
+
+class bidiselectionCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		selectionSet = self.view.sel()
+		for selectionRegion in selectionSet:
+			bidiRegion(selectionRegion, self.view, edit)
+
+def bidiRegion(region, view, edit):
+	txt = view.substr(region)
+	reshaped_text = arabic_reshaper.reshape(txt)
+	view.replace(edit, region, reshaped_text)
